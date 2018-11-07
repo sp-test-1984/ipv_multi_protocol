c28ebb8ff6edfd112d7aff514d2f3890412b1626
Juan Carlos Hernandez Iñiguez <juanhernandez@Juans-MacBook-Pro-2.local>
2018-11-07 11:49:39 -0600

    added logger dependencies on pom.xml (slf4j)

--- ipv/src/test/java/stackpath/util/TestConf.java
+++ ipv/src/test/java/stackpath/util/TestConf.java
@@ -2,17 +2,23 @@ package stackpath.util;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 import java.util.Map;
 
 public class TestConf {
     private static Config BASE_CONFIG = ConfigFactory.load();
+    private static Logger LOGGER = LoggerFactory.getLogger(TestConf.class);
+
+
 
     public static void displayConfig(){
         Map<String, Object> unwrapped = BASE_CONFIG.root().unwrapped();
         for (String entry :
              unwrapped.keySet()) {
-            System.out.println("key value is: " + entry);
+            LOGGER.info(entry);
         }
     }
 }
